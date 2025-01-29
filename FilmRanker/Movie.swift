fd6caee19f1e79f1fe3f74443622c365da277a83
hamsvensson <ham.sve@gmail.com>
2025-01-29 16:05:04 +0100

    Update Movie.swift

--- FilmRanker/Movie.swift
+++ FilmRanker/Movie.swift
@@ -1,18 +1,13 @@
 import Foundation
 
-struct Movie: Identifiable, Codable, Equatable {
-    let id: String
-    let title: String
-    let year: String
-    let director: String
-    let poster: String
+struct Movie: Identifiable, Codable {
+    var id: String
+    var title: String
+    var year: String
+    var director: String
+    var poster: String
     var rank: Int?
     var watchDate: Date?
     var review: String?
-    var isRankingComplete: Bool = false
-
-    static func == (lhs: Movie, rhs: Movie) -> Bool {
-        return lhs.id == rhs.id
-    }
+    var isRankingComplete: Bool
 }
-
